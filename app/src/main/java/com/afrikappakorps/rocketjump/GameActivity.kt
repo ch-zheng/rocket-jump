448794546237812a8193627d1d4bc61b2175c298
Charlie Zheng <chzheng@protonmail.com>
2019-08-02 21:15:13 -0500

    Initial commit

---
+++ app/src/main/java/com/afrikappakorps/rocketjump/GameActivity.kt
@@ -0,0 +1,12 @@
+package com.afrikappakorps.rocketjump
+
+import android.app.Activity
+import android.os.Bundle
+
+class GameActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
---
+++ app/src/main/java/com/afrikappakorps/rocketjump/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.afrikappakorps.rocketjump
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.play_button).setOnClickListener { startActivity(Intent(this, GameActivity::class.java)) }
+    }
+}
